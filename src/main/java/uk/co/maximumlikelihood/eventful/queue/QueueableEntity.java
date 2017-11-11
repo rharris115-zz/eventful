9d53ebf672f07c8390ae334ffcdd99ca28a79d56
rich <richard.w.harris@mac.com>
2017-11-11 16:17:19 +0000

    small things

--- src/main/java/uk/co/maximumlikelihood/eventful/queue/QueueableEntity.java
+++ src/main/java/uk/co/maximumlikelihood/eventful/queue/QueueableEntity.java
@@ -2,11 +2,15 @@ package uk.co.maximumlikelihood.eventful.queue;
 
 public interface QueueableEntity<E extends QueueableEntity<E, T>, T extends Comparable<? super T>> {
 
-    void notifyQueueArrival(EntityQueue<E, T> queue, T time);
+    default void notifyQueueArrival(EntityQueue<E, T> queue, T time) {
+    }
 
-    void notifyEnteringQueue(EntityQueue<E, T> queue, T time);
+    default void notifyEnteringQueue(EntityQueue<E, T> queue, T time) {
+    }
 
-    void notifyQueuePostProcessingStart(EntityQueue<E, T> queue, T time);
+    default void notifyQueuePostProcessingStart(EntityQueue<E, T> queue, T time) {
+    }
 
-    void notifyQueuePostProcessingFinish(EntityQueue<E, T> queue, T time);
+    default void notifyQueuePostProcessingFinish(EntityQueue<E, T> queue, T time) {
+    }
 }
